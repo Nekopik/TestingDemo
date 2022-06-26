b06925c882f4ff3c2c360696b8d028d1d436d985
Nekopik <sebekbattlefield@gmail.com>
2022-06-26 21:32:12 +0200

    Dodaj pliki projektów.

---
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(4, 3, 7)]
+        [InlineData(21, 5.25, 26.25)]
+        [InlineData(43, 35.2, 78.2)]
+        [InlineData(21.25, 11.75, 33)]
+        [InlineData(double.MaxValue, 5, double.MaxValue)]
+        public void Add_SimpleValuesShouldCalculate(double x, double y, double expected)
+        {
+            // Act
+            double actual = Calculator.Add(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(8,4,2)]
+        [InlineData(222, 5, 44.4)]
+        public void Divide_SimpleValuesShouldCalculate(double x, double y, double expected)
+        {
+            // Act
+            double actual = Calculator.Divide(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Divide_DivideByZero()
+        {
+            // Arrange
+            double expected = 0;
+
+            // Act
+            double actual = Calculator.Divide(15, 0);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
